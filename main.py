c5f2ab45db57202abf09a0d50fe6a68aeac361cf
tech511 <safinulla7@gmail.com>
2026-07-28 01:10:06 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import turtle
 from settings import *
 from stars import StarField
 from player import Player
+from bullet import Bullet
+from enemy import Enemy
+from explosion import Explosion
+from collision import check_collisions
+from score import Score
 
 # Screen
 screen = turtle.Screen()
@@ -15,15 +20,32 @@ screen.tracer(0)
 # Objects
 stars = StarField()
 player = Player()
+bullet = Bullet()
+enemy = Enemy()
+explosion = Explosion()
+score = Score()
 
 # Keyboard
 screen.listen()
 screen.onkeypress(player.move_left, "Left")
 screen.onkeypress(player.move_right, "Right")
 
-# Game Loop
+# Game Loops 
 while True:
 
     stars.update()
+
+    bullet.update()
+
+    enemy.update()
+
+    explosion.update()
+
+    check_collisions(
+        bullet,
+        enemy,
+        explosion,
+        score
+    )
 
     screen.update()
